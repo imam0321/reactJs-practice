624ac25bb41ca39496dfd91cd0821c34508d2343
imam0321 <imam.hossain0321@gmail.com>
2025-01-07 02:51:34 +0600

    test branch

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+import Test from "./components/Test";
+
 function App() {
   return (
     <>
       <h1 className="text-4xl font-bold text-center my-10">ReactJs Practice</h1>
+      <Test/>
     </>
   );
 }
