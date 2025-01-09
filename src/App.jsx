21de881c76f8745ab009aa87d868f28f5f21272e
imam0321 <imam.hossain0321@gmail.com>
2025-01-09 23:04:37 +0600

    add new branch 3.13-TSReducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,56 @@
+import { useState } from "react";
+import AddTask from "./components/AddTask";
+import TaskList from "./components/TaskList";
+import { initialTasks } from "./data/tasks";
+
 function App() {
+  const [tasks, setTasks] = useState(initialTasks);
+
+  const getNextId = (data) => {
+    const maxId = data.reduce((prev, currentId) =>
+      prev && prev.id > currentId ? prev.id : currentId
+    );
+    return maxId + 1;
+  };
+
+  // handler
+  const handleAddTask = (text) => {
+    setTasks([
+      ...tasks,
+      {
+        id: getNextId(tasks),
+        text: text,
+        done: false,
+      },
+    ]);
+  };
+
+  const handleChangeTask = (task) => {
+    const nextTask = tasks.map((t) => {
+      if (t.id === task.id) {
+        return task;
+      } else {
+        return t;
+      }
+    });
+
+    setTasks(nextTask);
+  };
+
+  const handleDeleteTask = (taskId) => {
+    setTasks(tasks.filter((t) => t.id !== taskId));
+  };
+
   return (
-    <>
-      <h1 className="text-4xl font-bold text-center my-10">ReactJs Practice</h1>
-    </>
+    <div className="m-10">
+      <h1 className="text-2xl m-2">Prague itinerary</h1>
+      <AddTask onAdd={handleAddTask} />
+      <TaskList
+        tasks={tasks}
+        onChangeTask={handleChangeTask}
+        onDeleteTask={handleDeleteTask}
+      />
+    </div>
   );
 }
 
