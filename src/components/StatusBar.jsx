0d595292a5a24ccb33f35e18ba38b44086caae51
imam0321 <imam.hossain0321@gmail.com>
2025-01-15 00:40:34 +0600

    create custom hook

--- src/components/StatusBar.jsx
+++ src/components/StatusBar.jsx
@@ -1,25 +1,8 @@
-import { useEffect, useState } from "react";
+import useOnlineStatus from "../hooks/useOnlineStatus";
+
 
 export default function StatusBar() {
-  const [isOnline, setIsOnline] = useState(true);
-
-  const handleOnline = () => {
-    setIsOnline(true);
-  };
-
-  const handleOffline = () => {
-    setIsOnline(false);
-  };
-
-  useEffect(() => {
-    window.addEventListener("online", handleOnline);
-    window.addEventListener("offline", handleOffline);
-
-    return () => {
-      window.removeEventListener("online", handleOnline);
-      window.removeEventListener("offline", handleOffline);
-    };
-  }, []);
+  const isOnline = useOnlineStatus();  
 
   return <h1 className="text-3xl font-semibold my-4">{isOnline ? "✅ Online" : "❌ Disconnected"}</h1>;
 }
