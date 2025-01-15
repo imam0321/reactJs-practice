eb5c1db2186f68e6262e5dfa0541a6352daadcbe
imam0321 <imam.hossain0321@gmail.com>
2025-01-15 19:07:16 +0600

    practice react router in react tutorial

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,39 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Root from "./Root";
+import ErrorPage from "./Error";
+import Contact from "./Contact";
+import { getContactLoader, getContactsLoader } from "./loaders/ContactsLoader";
+import { createContactAction, editContactAction } from "./actions/ContactsAction";
+import EditContact from "./EditContact";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    loader: getContactsLoader,
+    action: createContactAction,
+    children: [
+      {
+        path: "/contacts/:contactId",
+        element: <Contact />,
+        loader: getContactLoader,
+      },
+      {
+        path: "/contacts/:contactId/edit",
+        element: <EditContact />,
+        loader: getContactLoader,
+        action: editContactAction
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
